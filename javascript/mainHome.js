79c30a13021731c3d20bd5cdaa0a7ed88cbbdca1
Dovilė <dovile.keparute@gmail.com>
2021-09-14 18:59:36 +0300

    Structure-update

--- javascript/mainHome.js
+++ javascript/mainHome.js
@@ -1,15 +1,33 @@
 // IMPORT
-// .......................
-// header
-// services
-// achievements
-// footer
+
+/* header */
+/* hero */
+/* clients */
+/* services */
+/* about company */
+/* hire us */
+/* chievements */
+/* promo video */
+/* case studies */
+/* testimonials */
+/* blog */
+/* contacts */
+/* footer */
 
 // FUNKCIJU PANAUDOJIMAS
-// ....................
-// header
-// services
-// achievements
-// footer
+
+/* header */
+/* hero */
+/* clients */
+/* services */
+/* about company */
+/* hire us */
+/* chievements */
+/* promo video */
+/* case studies */
+/* testimonials */
+/* blog */
+/* contacts */
+/* footer */
 
 // EXPORT
