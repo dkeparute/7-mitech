c11d047fe29bda13b01a9c522e54950fa5235375
Dovilė <dovile.keparute@gmail.com>
2021-09-29 21:26:36 +0300

    Services

--- javascript/components/services/services.js
+++ javascript/components/services/services.js
@@ -8,26 +8,34 @@ function services(selector, data) {
     // const servicesArray = data.list;
     // const imgPath = data.imgPath; 
     // const maxCount = data.maxCount;
- 
+
     // VISA TAI GALIMA PERRASYTI I PAPRASTESNI VARIANTA:
 
-   const {list, maxCount, imgPath} = data;
+    const { list, maxCount, imgPath } = data;
+
 
     let HTML = "";
+    let generatedServicesCount = 0;
 
     for (let i = 0; i < list.length; i++) {
         const service = list[i];
- 
+
         if (!service.active) {
             continue;
         }
+        // SUKURTAS RODIKLIS JOG TURETUME RIBOTA KIEKI DATA DUOMENU
+        if (generatedServicesCount === maxCount) {
+            break;
+        }
+        generatedServicesCount++;
 
-    HTML += ` <div class="col-12 col-md-6 col-lg-4 service-item">
+        HTML += ` <div class="service-item">
     <img src="${imgPath + service.img}" alt="">
     <h4>${service.title}</h4>
     <p>${service.description}</p>
     <a href="${service.link}" class="fa fa-arrow-right" aria-hidden="true"></a>
-    </div>`;}
+    </div>`;
+    }
 
     // post logic validation
 
