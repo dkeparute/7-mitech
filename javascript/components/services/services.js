f2acae3031dd699ff8ec668e5863ebec602327cc
Dovilė <dovile.keparute@gmail.com>
2021-09-29 19:58:12 +0300

    .

--- javascript/components/services/services.js
+++ javascript/components/services/services.js
@@ -2,14 +2,41 @@
 function services(selector, data) {
     // input validation
 
+
     // logika
-    // DOM - HTML ELEMENTU STRUKTURA TIK JAVASCRIPTE
-    // DOKUMENTE IESKOSIME NURODYTOS VIETOS
     const DOM = document.querySelector(selector);
-    console.log(DOM);
+    const servicesArray = data.list;
+    const imgPath = data.imgPath; 
+
+    let HTML = "";
+
+    for (let i = 0; i < servicesArray.length; i++) {
+        const service = servicesArray[i];
+ 
+
+        if (!service.active) {
+            continue;
+        }
+        HTML += ` <div class="col-12 col-md-6 col-lg-4 service-item">
+    <img src="${imgPath + service.img}" alt="">
+    <h4>${service.title}</h4>
+    <p>${service.description}</p>
+    <a href="${service.link}" class="fa fa-arrow-right" aria-hidden="true"></a></div>`;
+    }
+
+
+
+
+
+
+
+
     // post logic validation
 
+
     // rezultato grazinimas, sugeneruoto HTML istatymas
+    DOM.innerHTML = HTML;
+
 
 }
 
