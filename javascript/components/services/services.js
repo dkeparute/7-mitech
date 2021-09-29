534d58003394c58d244beb441abc0f67d894c942
Dovilė <dovile.keparute@gmail.com>
2021-09-29 18:57:31 +0300

    .

--- javascript/components/services/services.js
+++ javascript/components/services/services.js
@@ -1,6 +1,15 @@
 // // jei pirma raide mazoji tai bus funkcinis programavimo stilius
-function services(data) {
-    console.log(data);
+function services(selector, data) {
+    // input validation
+
+    // logika
+    // DOM - HTML ELEMENTU STRUKTURA TIK JAVASCRIPTE
+    // DOKUMENTE IESKOSIME NURODYTOS VIETOS
+    const DOM = document.querySelector(selector);
+    console.log(DOM);
+    // post logic validation
+
+    // rezultato grazinimas, sugeneruoto HTML istatymas
 
 }
 
