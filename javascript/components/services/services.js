dc0084e9714d4e89aebc9417ac808592eb3a9361
Dovilė <dovile.keparute@gmail.com>
2021-09-29 20:11:31 +0300

    Services

--- javascript/components/services/services.js
+++ javascript/components/services/services.js
@@ -5,36 +5,34 @@ function services(selector, data) {
 
     // logika
     const DOM = document.querySelector(selector);
-    const servicesArray = data.list;
-    const imgPath = data.imgPath; 
+    // const servicesArray = data.list;
+    // const imgPath = data.imgPath; 
+    // const maxCount = data.maxCount;
+ 
+    // VISA TAI GALIMA PERRASYTI I PAPRASTESNI VARIANTA:
+
+   const {list, maxCount, imgPath} = data;
 
     let HTML = "";
 
-    for (let i = 0; i < servicesArray.length; i++) {
-        const service = servicesArray[i];
+    for (let i = 0; i < list.length; i++) {
+        const service = list[i];
  
-
         if (!service.active) {
             continue;
         }
-        HTML += ` <div class="col-12 col-md-6 col-lg-4 service-item">
+
+    HTML += ` <div class="col-12 col-md-6 col-lg-4 service-item">
     <img src="${imgPath + service.img}" alt="">
     <h4>${service.title}</h4>
     <p>${service.description}</p>
-    <a href="${service.link}" class="fa fa-arrow-right" aria-hidden="true"></a></div>`;
-    }
-
-
-
-
-
-
-
+    <a href="${service.link}" class="fa fa-arrow-right" aria-hidden="true"></a>
+    </div>`;}
 
     // post logic validation
 
 
-    // rezultato grazinimas, sugeneruoto HTML istatymas
+    // return result - rezultato grazinimas, sugeneruoto HTML istatymas
     DOM.innerHTML = HTML;
 
 
